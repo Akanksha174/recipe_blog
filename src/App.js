49fa0d0cf7fd51bbb553ac27421d9f0b521afb76
Akanksha174 <akankshakumari4755@gmail.com>
2024-11-14 12:56:25 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Books from "./pages/Book/Book";
-
+import BookDetails from "./pages/Detail/BookDetail";
 
 const App=()=>{
     return(
@@ -10,6 +10,7 @@ const App=()=>{
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path="/books" element={<Books/>}></Route>
+                <Route path="/books-details/:id" element={<BookDetails/>}></Route>
             </Routes>
             </div>
     )
---
+++ src/pages/Detail/BookDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BookDetails=()=>{
+    return(
+        <section>
+            <h1>BookPage</h1>
+        </section>
+    )
+}
+export default BookDetails;
